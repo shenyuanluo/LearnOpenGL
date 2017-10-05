077247985dd71ca7efecd0b486f408f4a071885b
shenyuanluo <shenyuanluo@163.com>
2017-10-05 23:20:44 +0800

    添加 Transformations Demo工程
    添加 Transformations Demo工程代码

---
+++ Transformations/README.md
@@ -0,0 +1,5 @@
+# LearnOpenGL
+
+> 矩阵变换。
+
+[Transformations](https://learnopengl.com/#!Getting-started/Transformations) 教程的 **Demo**。
---
+++ Transformations/Transformations/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Transformations
+//
+//  Created by shenyuanluo on 2017/10/5.
+//  Copyright © 2017年 http://blog.shenyuanluo.com/ All rights reserved.
+//
+
+#include <iostream>
+#include "RenderView.h"
+
+
+int main()
+{
+    const GLuint scrWidth  = 800;
+    const GLuint scrHeight = 600;
+    const GLchar* scrTitle = "Transformations";
+    
+    RenderView renView(scrWidth, scrHeight, scrTitle);
+    
+    if (RetError == renView.initGlad())
+    {
+        return -1;
+    }
+    
+    // 定义 矩形顶点数据数组
+    GLfloat vertices[] =
+    {
+        // 位置坐标             // 颜色             // 纹理坐标
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,       // 右上顶点
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,       // 右下顶点
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,       // 左下顶点
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,       // 左上顶点
+    };
+    
+    renView.render(vertices, sizeof(vertices));
+    
+    return 0;
+}
